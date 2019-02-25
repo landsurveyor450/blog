e30de070df14b5be4be5c99e257fb8d991da1920
landsurveyor450git config --global user.email landsurveyor450@gmail.com <landsurveyor450@gmail.com>
2019-02-25 16:38:19 +0300

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,45 @@
+class ArticlesController < ApplicationController
+    before_action :authenticate_user!, :only => [:new, :create]
+    
+    def index
+        @article = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if  @article.update(article_params)
+                redirect_to @article
+        else
+            render action: 'edit'
+        end      
+    end
+    
+    
+    def new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+    
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
+end
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+    before_action :authenticate_user!, :only => [:new, :create]
+    def create
+        @article = Article.find(params[:article_id])
+    @article.comments.create(comment_params)
+    
+
+    redirect_to article_path(@article)
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:autor, :username, :body)
+    end
+end
